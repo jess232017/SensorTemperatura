e714bc1c09672a8b3c03fd192cf6263a9b8ca602
Jesus Hernandez <jess232016@gmail.com>
2021-04-29 16:31:25 -0600

    iniciando offline

--- admin/service-worker.js
+++ admin/service-worker.js
@@ -1,10 +1,56 @@
 // service-worker.js
-self.addEventListener('install', function() {
-    //console.log('Install!');
+self.addEventListener('install', function(event) {
+    // Instalar de inmediato
+    if (self.skipWaiting) { self.skipWaiting(); }
+    event.waitUntil(
+        caches.open('cache01').then(function(cache) {
+            return cache.addAll([
+                './',
+                //HTML
+                'attendance.html',
+                'index.html',
+                'login.js',
+                'student-info.html',
+                'student.html',
+                'temperature.html',
+                //JavaScript
+                './js/main.js',
+                './js/firebase.js',
+                //CSS
+                './css/style.css',
+                './css/login.css',
+                './css/style.css',
+                './css/student.css',
+            ]);
+        })
+    );
 });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Elimina archivos de cache viejos
+var cacheWhitelist = ['cache01'];
+caches.keys().then(function(cacheNames) {
+    return Promise.all(
+        cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+            }
+        })
+    );
+});
+
+caches.keys().then(function(cacheKeys) {
+    // Muestra en la consola la cache instalada 
+    console.log('Versión SW: ' + cacheKeys);
+});
+
 self.addEventListener("activate", event => {
     //console.log('Activate!');
-});
-self.addEventListener('fetch', function(event) {
-    //console.log('Fetch!', event.request);
 });
